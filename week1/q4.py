556fc9ece2a35c6e3b1844fa750694c4e98f8a02
SRIVISHWA-P <srivichu308@gmail.com>
2022-11-16 13:58:38 +0530

    sltered q4

--- week1/q4.py
+++ week1/q4.py
@@ -3,5 +3,8 @@
 a=int(input("Enter the first term of the series 'a' :"))
 r=int(input("Enter the common ratio of the series 'r' :"))
 n=int(input("Enter the n value(not zero):"))
-term=a*(pow(r,n-1))
-print("\nThe %dth term of the series is %d"%(n,term))
+print("\nThe series is :")
+count=1
+while(count!=n+1):
+    print(a*(pow(r,count-1)),end=' ')
+    count+=1
