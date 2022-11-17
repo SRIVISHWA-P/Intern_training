d0d4f13899361539677f94b8840811b0bbbc385b
SRIVISHWA-P <srivichu308@gmail.com>
2022-11-17 20:14:32 +0530

    spellcheck

--- week1/q9.py
+++ week1/q9.py
@@ -5,4 +5,4 @@ count=0
 for i in s:
     if((i>'A' and i<'Z') or (i>'a' and i<'z')):
         count+=1
-print("The nukber of alphabets in the given string is %d"%count)
+print("The number of alphabets in the given string is %d"%count)
